5a164a483c4341faaf8b865a2d02011aca659645
JiangshuoZhao <1787076149@qq.com>
2023-11-14 14:44:58 +0800

    first commit

---
+++ Readme.md
@@ -0,0 +1,24 @@
+# cppreference 外置手册(U-tools专用)
+本手册依赖[uTools效率工具](http://www.u.tools/)及其插件[程序员手册](https://yuanliao.info/d/356-1-1-4-bug-tmux)开发。
+## 手册制作
+1. 相关html文档来自于[https://github.com/myfreeer/cppreference2mshelp/releases](https://github.com/myfreeer/cppreference2mshelp/releases)
+2. 使用Python脚本`doc_catalog.py`生成适用于[程序员手册](https://yuanliao.info/d/356-1-1-4-bug-tmux)的目录文件；
+## 手册优点：
+1. 借助Utools可实现API速查
+2. 可自行下载最新的cppreference中文文档，并制作目录
+## 使用方法：
+1. 下载并安装[uTools效率工具](http://www.u.tools/)；
+2. 下载[c++ 外置手册](https://github.com/Nothing1024/Pytorch-handbook-Utools/releases)并解压；
+3. 在[uTools效率工具](http://www.u.tools/)的插件市场中安装`程序员手册`插件；
+4. 在手册插件的`手册设置`中添加手册，根据需要配置名称，关键字，说明，路径（解压后手册的绝对路径）；
+5. 点击`保存`，打开手册开关。
+## 待改进：
+1. css格式未修改，显示的结果可能不太理想
+2. 虽然添加了命名空间的索引,可用std::vector查找，但有重复的现象，最终的name是std::std::vector
+3. 不能链接跳转，例如查找std::vector,想查看其成员函数push_back，不能跳转push_back的文档
+## 动机
+虽然uTools中有一个C++函数速查插件，但文档未更新，还是c++20的,c++23的未包含。使用[程序员手册](https://yuanliao.info/d/356-1-1-4-bug-tmux)可以自己定制相关的文档，难点在于获取文档及制作目录。
+cppreference的文档可在[https://github.com/myfreeer/cppreference2mshelp/releases](https://github.com/myfreeer/cppreference2mshelp/releases)获取，主要在于制作目录。
+
+自己用，先考虑的是快速使用，缺点很多，有时间后续会先完善链接跳转的功能。
+
---
+++ doc_catalog.py
@@ -0,0 +1,62 @@
+import requests
+import os
+from glob import glob
+import json
+from pyquery import PyQuery as pq
+
+def find_html_files(root_folder):
+    html_files = []
+    for root, dirs, files in os.walk(root_folder):
+        for file in files:
+            if file.endswith(".html"):
+                html_files.append(os.path.join(root, file)[42:])
+                #这里取42及之后的是为了获取相对路径，json文件的path属性使用绝对路径不能用
+                #根据自己的存放地址变动
+                #例如我的放在D:\c++doc\html-book-20231001\reference\zh\文件下
+    return html_files
+
+def get_catalog(path):
+    with open(path,'r',encoding='utf-8') as file:
+        html_content = file.read()
+        fun_name = ''
+        try:
+            # 获取文档描述信息
+            doc = pq(html_content)
+            name = doc('#firstHeading')
+            print(name)
+            prev = name('span')
+            if(prev):
+                fun_name += prev.text()
+
+            fun_name +=name.text()
+            print(fun_name)
+            fun_desc = fun_name
+        except:
+            pass
+
+    fun_setting = {
+                        "name": fun_name,
+                        "type": "cpp",
+                        "path": path,
+                        "desc": fun_desc
+                    }
+    return fun_setting
+
+
+
+#
+if __name__ == '__main__':
+    current_path = os.getcwd()
+    # 调用函数查找所有 .html 后缀的文件路径
+    html_file_paths = find_html_files(current_path)
+    print(html_file_paths)
+
+    fun_settings = []
+    # 提取信息，制作目录
+    for file_path in html_file_paths:
+        fun_settings.append(get_catalog(file_path))
+
+
+    #写入json文件中
+    with open('./cpp.json', 'w', encoding='utf-8') as fp:
+        json.dump(list(fun_settings), fp)
